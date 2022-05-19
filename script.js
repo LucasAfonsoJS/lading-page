e22aadf0d004dc3380ddc98416dc37ea9629ccd3
LucasAfonsoJS <100942581+LucasAfonsoJS@users.noreply.github.com>
2022-05-19 16:41:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('li', { duration: 1000 });
+sr.reveal('.container-single', { 
+    distance: '60px', 
+    origin: 'left',
+    duration: 1000 
+ });
+sr.reveal('.center', { 
+    duration: 1000 
+});
